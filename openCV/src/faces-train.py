5a38071aa6012cfe6805f72fb6a3aa8b1dab5fb8
Fadoua Massaoudy <fadoua.massaoudy@gmail.com>
2021-09-02 19:29:13 +0100

    second commit

--- openCV/src/faces-train.py
+++ openCV/src/faces-train.py
@@ -1,10 +1,59 @@
-import os
-BASE_DIR=os.path.dirname(os.path.abspath(__file__))
-image_dir=os.path.join(BASE_DIR,"images")
+import cv2
+import os 
+from PIL import Image
+import numpy as np
+import pickle #the process of converting a Python object into a byte stream to store it in a file/database
+#The OS module in Python provides functions for interacting with the operating system
+#creating and removing a directory (folder), fetching its contents, changing and identifying the current directory, etc
 
+BASE_DIR=os.path.dirname(os.path.abspath(__file__)) #method in Python is used to get the directory name from the specified path.
+image_dir=os.path.join(BASE_DIR,"images") #combines one or more path names into a single path
+
+face_cascade=cv2.CascadeClassifier("openCV\src\cascades\data\haarcascade_frontalface_alt2.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+current_id=0 # we want to associate an id to every label
+label_ids = {} #dictionnary
+y_labels = []
+x_train = []
+
+#root : Prints out directories only from what you specified.
+#dirs : Prints out sub-directories from root.
+#files : Prints out all files from root and directories.
 
 for root , dirs , files , in os.walk(image_dir):
     for file in files :
         if file.endswith("png") or file.endswith("jpg") :
             path = os.path.join(root , file)
-            print(path)
+            label=os.path.basename(root).replace(" " , "-").lower() 
+            print(label,path)
+
+            if not label in label_ids :
+                label_ids[label]=current_id
+                current_id += 1
+            id_=label_ids[label]
+            print(label_ids)
+            pil_image=Image.open(path).convert("L") # L for luminance : grayscale
+            #size=(550,550)
+            #final_image=pil_image.resize(size,Image.ANTIALIAS)
+            image_array=np.array(pil_image , "uint8")
+            #print(image_array)
+
+            faces=face_cascade.detectMultiScale(image_array,scaleFactor=1.5,minNeighbors=5)
+
+            for (x,y,h,w) in faces:
+                roi=image_array[y:y+h , x:x+w]
+                x_train.append(roi)
+                y_labels.append(id_) #a face should have only one id
+
+#print(y_labels)
+#print(x_train)
+
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids , f)
+
+recognizer.train(x_train,np.array(y_labels))
+recognizer.save("trainner.yml")
+
+
+
--- openCV/src/faces.py
+++ openCV/src/faces.py
@@ -1,7 +1,17 @@
 import numpy as np
 import cv2
+import pickle
 
 face_cascade=cv2.CascadeClassifier("openCV\src\cascades\data\haarcascade_frontalface_alt2.xml")
+#eye_cascade=cv2.CascadeClassifier("openCV\src\cascades\data\haarcascade_eye.xml")
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels ={}
+with open("labels.pickle", 'rb') as f:
+    og_labels=pickle.load(f)
+    labels={v:k for k,v in og_labels.items()}
 
 cap=cv2.VideoCapture(0) #This will return video from the first webcam on your computer. (capture)
 
@@ -23,9 +33,19 @@ while(True):
     # of the detected faces: higher value results in less detections 
     # but with higher quality
     for (x,y,w,h) in faces:
-        print(x,y,w,h)
-        roi_gray=gray[y:y+h , x:x+w] ##This particular code will return the cropped face from the image.
+        #print(x,y,w,h)
+        roi_gray=gray[y:y+h , x:x+w] ##This particular code will return the cropped face from the image
         roi_color=frame[y:y+h , x:x+w]
+        #reconize?
+        id_ ,conf=recognizer.predict(roi_gray)
+        if conf>=30 : # and conf<=100:
+            #print(id_)
+            #print(labels[id_])
+            font=cv2.FONT_HERSHEY_SIMPLEX
+            name=labels[id_]
+            color=(255,255,255)
+            stroke=2
+            cv2.putText(frame,name,(x,y),font,1,color,stroke,cv2.LINE_AA)
         img_item= "my-image.png"
         cv2.imwrite(img_item,roi_gray) #Syntax: cv2.imwrite(filename(must include image format), image)
         
@@ -35,6 +55,10 @@ while(True):
         end_cord_x=x+w
         end_cord_y=y+h
         cv2.rectangle(frame , (x,y) , (end_cord_x,end_cord_y),color,stroke)
+
+        #eyes=eye_cascade.detectMultiScale(roi_gray)
+        #for (ex,ey,eh,ew) in eyes :
+           # cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
     #display the resulting frame
     cv2.imshow("frame",frame)
